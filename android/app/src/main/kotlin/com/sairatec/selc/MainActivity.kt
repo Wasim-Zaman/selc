1d62e541586d5fdeb6aadd3f74d7238010651b61
Wasim Zaman <wasimxaman13@gmail.com>
2024-10-02 10:22:21 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/sairatec/selc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sairatec.selc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/themes.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  // Light Theme
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+    buttonTheme: const ButtonThemeData(
+      buttonColor: Colors.blue, // Primary color of buttons
+      textTheme: ButtonTextTheme.primary,
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.black),
+      bodySmall: TextStyle(color: Colors.black54),
+    ),
+  );
+
+  // Dark Theme
+  static final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blueGrey,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+    buttonTheme: const ButtonThemeData(
+      buttonColor: Colors.blueGrey,
+      textTheme: ButtonTextTheme.primary,
+    ),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.white),
+      bodySmall: TextStyle(color: Colors.white70),
+    ),
+  );
+}
