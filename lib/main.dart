90914b4cae0c4aa918d5ee255a56f3efc5788203
faysalzaman <faysalzaman00@gmail.com>
2024-10-02 11:45:47 +0500

    Refactor asset paths and add Firebase Core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:selc/utils/themes.dart';
 import 'package:selc/view/screens/user/auth/login_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SELC',
+      debugShowCheckedModeBanner: false,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
       home: const LoginScreen(),
