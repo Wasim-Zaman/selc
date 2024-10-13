290b9a328f7b177e991c0cf39d37a44eea262828
Wasim Zaman <wasimxaman13@gmail.com>
2024-10-13 13:47:10 +0500

    feat: Update navigation methods with page transitions

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Navigations {
-  // Method to push a new screen (without removing the previous one)
   static void push(BuildContext context, Widget screen) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => screen),
+    Navigator.push(
+      context,
+      PageTransition(
+        type: PageTransitionType.fade,
+        child: screen,
+        duration: const Duration(milliseconds: 300),
+        reverseDuration: const Duration(milliseconds: 300),
+      ),
     );
   }
 
-  // Method to replace the current screen (removes the previous screen)
   static void pushReplacement(BuildContext context, Widget screen) {
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => screen),
+    Navigator.pushReplacement(
+      context,
+      PageTransition(
+        type: PageTransitionType.rightToLeftWithFade,
+        child: screen,
+        duration: const Duration(milliseconds: 300),
+        reverseDuration: const Duration(milliseconds: 300),
+      ),
     );
   }
 
-  // Method to pop the current screen (go back)
   static void pop(BuildContext context) {
     Navigator.of(context).pop();
   }
 
-  // Method to push and remove all previous routes (used for logging out or resetting the app)
   static void pushAndRemoveUntil(BuildContext context, Widget screen) {
-    Navigator.of(context).pushAndRemoveUntil(
-      MaterialPageRoute(builder: (context) => screen),
-      (Route<dynamic> route) => false, // Remove all previous routes
+    Navigator.pushAndRemoveUntil(
+      context,
+      PageTransition(
+        type: PageTransitionType.fade,
+        child: screen,
+        duration: const Duration(milliseconds: 300),
+      ),
+      (Route<dynamic> route) => false,
     );
   }
 
-  // Method to navigate back to the previous screen with a result
   static void popWithResult<T>(BuildContext context, T result) {
     Navigator.of(context).pop(result);
   }
